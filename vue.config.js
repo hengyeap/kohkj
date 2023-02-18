c8ed03959eafea04abf4632f8b180feeff64421a
koh <kohhengyeap@gmail.com>
2023-02-18 19:13:45 +0800

    tuned

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/kongji/'
+    : '/',
   transpileDependencies: true,
-  publicPath: '/kongji/',
+  // publicPath: '/kongji/',
   lintOnSave:false
 });
+
