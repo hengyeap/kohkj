2a55f751115c6fd2e78ffec883cacb31c384b39c
koh <kohhengyeap@gmail.com>
2023-02-18 19:33:59 +0800

    index test

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/kongji/'
-    : '/',
+    : '/'
+    ? '/kongji/about'
+    :'/about',
   transpileDependencies: true,
   // publicPath: '/kongji/',
   lintOnSave:false
